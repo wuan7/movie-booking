04e8a79d6ed5cde34a9f0fc0566c7ec7cdf1c73f
wuan7 <wuan.work@gmail.com>
2024-11-06 16:46:55 +0700

    update url config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['deafening-retriever-318.convex.cloud', 'github.com', 'movie-booking-six.vercel'],
+        domains: ['deafening-retriever-318.convex.cloud', 'github.com', 'movie-booking-six.vercel', 'cool-turtle-145.convex.cloud'],
       },
 };
 
